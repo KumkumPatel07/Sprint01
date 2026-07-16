620405079fcf922e99e3ebacfc7b2a77a9ebc7b3
KumkumPatel07 <kumkumpatel071749@gmail.com>
2026-07-17 01:21:31 +0530

    Pre finale

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-const btn= document.getElementById("theme-btn");
-btn.onclick = function(){
-    document.body.classList.toggle("dark");
-}
+const icon = document.getElementById("icon");
 
+icon.onclick = function () {
+
+  document.body.classList.toggle("dark-theme");
+
+  if (document.body.classList.contains("dark-theme")) {
+
+    icon.classList.remove("fa-moon");
+    icon.classList.add("fa-sun");
+  } else {
+    icon.classList.remove("fa-sun");
+    icon.classList.add("fa-moon");
+  }
+};
